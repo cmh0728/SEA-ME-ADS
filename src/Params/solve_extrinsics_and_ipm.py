2c1bcf28a3419d43d7d9df2818f65655380be371
cmh0728 <gambaro344@gmail.com>
2025-11-13 12:57:40 +0100

    edit frame

--- src/Params/solve_extrinsics_and_ipm.py
+++ src/Params/solve_extrinsics_and_ipm.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 # ---------- 사용자 설정 ----------
-IMAGE_PATH = "frame1.jpg"      # 바닥 체스보드가 보이는 한 장
+IMAGE_PATH = "frame0001.jpg"      # 바닥 체스보드가 보이는 한 장
 # 내부 코너 수(가로 x 세로) - 인쇄물 내부코너 기준으로 맞춰주세요!
 BOARD_COLS, BOARD_ROWS = 9, 7
 SQUARE_SIZE_M = 0.011  # 1.1 cm
