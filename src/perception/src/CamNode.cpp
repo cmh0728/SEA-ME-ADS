71fb8e74a3be56af431d17570e00abb57515331f
cmh0728 <gambaro344@gmail.com>
2025-11-20 12:21:52 +0100

    check margin Y size2

--- src/perception/src/CamNode.cpp
+++ src/perception/src/CamNode.cpp
@@ -684,7 +684,7 @@ void SlidingWindow(const cv::Mat& st_EdgeImage,
     // 윈도우 파라미터
     int32_t s32_WindowHeight = rows - 1;        // 현재 윈도우의 "아래쪽" y (맨 아래에서 시작)
     const int32_t s32_MarginX = 30;             // 윈도우 가로 반폭
-    const int32_t s32_MarginY = 80;             // 윈도우 세로 높이
+    const int32_t s32_MarginY = 70;             // 윈도우 세로 높이
     int32_t s32_I, s32_CentorX, s32_CentorY;
 
     // 유효 윈도우 유지 여부
