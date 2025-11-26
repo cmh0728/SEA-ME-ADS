1d77ceab5e5c4f19210dc4c5d398bc6921fa4ae8
cmh0728 <gambaro344@gmail.com>
2025-11-26 21:39:05 +0100

    add original pid controller

---
+++ src/control/include/control/Pid_control.hpp
@@ -0,0 +1,42 @@
+#ifndef Pid_control__CONTROL_NODE_HPP_
+#define Pid_control__CONTROL_NODE_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+namespace control
+{
+// 차선 중심 오프셋을 받아 PID로 조향 각을 계산하고 /cmd_vel을 퍼블리시합니다.
+class ControlNode : public rclcpp::Node
+{
+public:
+  ControlNode();
+
+private:
+  void on_offset(const std_msgs::msg::Float32::SharedPtr msg);
+  void on_heading(const std_msgs::msg::Float32::SharedPtr msg);
+  void reset_if_timeout(const rclcpp::Time & now);
+
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr offset_sub_;
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr heading_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+
+  double kp_;
+  double ki_;
+  double kd_;
+  double max_integral_;
+  double max_angular_z_;
+  double linear_speed_;
+  double pixel_to_meter_;
+  double integral_error_;
+  double prev_error_;
+  double heading_error_;
+  double heading_weight_;
+  double last_angular_cmd_;
+  rclcpp::Time last_stamp_;
+  rclcpp::Duration watchdog_timeout_;
+};
+}  // namespace control
+
+#endif  // CONTROL__CONTROL_NODE_HPP_
