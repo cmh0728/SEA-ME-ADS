00b0d1d4f10794cc8d40d9e31948f64bc849d8ac
cmh0728 <gambaro344@gmail.com>
2025-11-30 18:06:19 +0100

    edit recorder code

--- src/bag_recorder_nodes/src/simple_bag_recorder.cpp
+++ src/bag_recorder_nodes/src/simple_bag_recorder.cpp
@@ -1,10 +1,10 @@
+#include <memory>
+
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/serialization.hpp>
 #include <std_msgs/msg/string.hpp>
 
-#include <rosbag2_cpp/typesupport_helpers.hpp>
 #include <rosbag2_cpp/writer.hpp>
-#include <rosbag2_cpp/writers/sequential_writer.hpp>
-#include <rosbag2_storage/serialized_bag_message.hpp>
 
 using std::placeholders::_1;
 
@@ -14,52 +14,41 @@ public:
   SimpleBagRecorder()
   : Node("simple_bag_recorder")
   {
-    const rosbag2_cpp::StorageOptions storage_options({"my_bag", "sqlite3"});
-    const rosbag2_cpp::ConverterOptions converter_options(
-      {rmw_get_serialization_format(),
-       rmw_get_serialization_format()});
-    writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
+    // rosbag2 writer 생성
+    writer_ = std::make_unique<rosbag2_cpp::Writer>();
 
-    writer_->open(storage_options, converter_options);
+    // 그냥 이름만 주고 open (기본 storage 옵션: sqlite3)
+    writer_->open("my_bag");
 
-    writer_->create_topic(
-      {"chatter",
-       "std_msgs/msg/String",
-       rmw_get_serialization_format(),
-       ""});
-
-    subscription_ = create_subscription<std_msgs::msg::String>(
-      "chatter", 10, std::bind(&SimpleBagRecorder::topic_callback, this, _1));
+    // String 타입으로 평범하게 subscribe
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "chatter", 10,
+      std::bind(&SimpleBagRecorder::topic_callback, this, _1));
   }
 
 private:
-  void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
-    auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+    // 1) std_msgs::msg::String → rclcpp::SerializedMessage 로 직렬화
+    rclcpp::Serialization<std_msgs::msg::String> serializer;
+    rclcpp::SerializedMessage serialized_msg;
+    serializer.serialize_message(msg.get(), &serialized_msg);
 
-    bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
-      new rcutils_uint8_array_t,
-      [this](rcutils_uint8_array_t *msg) {
-        auto fini_return = rcutils_uint8_array_fini(msg);
-        delete msg;
-        if (fini_return != RCUTILS_RET_OK) {
-          RCLCPP_ERROR(get_logger(),
-            "Failed to destroy serialized message %s", rcutils_get_error_string().str);
-        }
-      });
-    *bag_message->serialized_data = msg->release_rcl_serialized_message();
+    // 2) 타임스탬프 (수신 시각 사용)
+    rclcpp::Time time_stamp = this->now();
 
-    bag_message->topic_name = "chatter";
-    if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
-      RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
-        rcutils_get_error_string().str);
-    }
-
-    writer_->write(bag_message);
+    // 3) bag에 쓰기
+    //    Writer::write(const rclcpp::SerializedMessage&, const std::string& topic,
+    //                  const std::string& type, const rclcpp::Time& time)
+    writer_->write(
+      serialized_msg,
+      "chatter",
+      "std_msgs/msg/String",
+      time_stamp);
   }
 
-  rclcpp::Subscription<rclcpp::SerializedMessage>::SharedPtr subscription_;
-  std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  std::unique_ptr<rosbag2_cpp::Writer> writer_;
 };
 
 int main(int argc, char * argv[])
